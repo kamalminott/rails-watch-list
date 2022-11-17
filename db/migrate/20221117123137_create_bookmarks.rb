08dabbcd052ce30d0dd19ae7eee9db50240ea0ae
Kamal Minott <kamalminott@gmail.com>
2022-11-17 14:51:41 +0000

    watch list

---
+++ db/migrate/20221117123137_create_bookmarks.rb
@@ -0,0 +1,10 @@
+class CreateBookmarks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookmarks do |t|
+      t.text :comment
+      t.belongs_to :movie, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
